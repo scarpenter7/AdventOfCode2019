72f28c787ad31b5992be0f6b93ad73c68bfa7be3
scarpenter7 <scarpenter7@outlook.com>
2020-12-21 19:20:01 -0800

    Day2 initial

--- Day2/day2.py
+++ Day2/day2.py
@@ -1,3 +1,27 @@
-print("hello")
-print("wassup")
-print("howd")
+
+
+def run(codes):
+
+    for num in range(0, len(codes), 4):
+        if codes[num] == 1:
+            codes[num + 3] = codes[num + 1] + codes[num + 2]
+        elif codes[num] == 2:
+            codes[num + 3] = codes[num + 1] * codes[num + 2]
+        elif codes[num] == 99:
+            return codes
+        else:
+            print("ERROR: " + str(codes[num]))
+
+    return codes
+
+if __name__ == "__main__":
+    codes = []
+    with open("code.txt", 'r') as myfile:
+        lines = myfile.readlines()
+        for line in lines:
+            codes += line.split(',')
+        codes = [int(num) for num in codes if num != '\n']
+    print(codes)
+    finalCodes = run(codes)
+
+
