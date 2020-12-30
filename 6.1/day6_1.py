3692dff7d57bee358e9a9e0d0bb6cc5351ebe493
scarpenter7 <scarpenter7@outlook.com>
2020-12-30 15:18:57 -0800

    Day 6.1

--- 6.1/day6_1.py
+++ 6.1/day6_1.py
@@ -1,49 +1,64 @@
 class Planet(object):
     def __init__(self, name, parentPlanet):
         self.name = name
-        self.parentPlanetName = parentPlanet
+        self.parentPlanet = parentPlanet
         self.childPlanets = set()
 
 class OrbitMap(object):
     def __init__(self):
         self.root = Planet("COM", None)
-        self.allPlanetNames = set("COM")
-        self.allTreeRoots = set(self.root)
-
-    def findPlanet(self, planetName):
-        for tree in self.allTreeRoots:
-            planet = self.searchPlanetTree(tree, planetName)
-            if planet is not None:
-                return planet
-        raise ValueError("Planet not found!")
-
-    def searchPlanetTree(self, tree, planetName):
-        #TODO finish this function
-        return 0
+        self.allPlanets = {}
+        self.allPlanets.update({"COM": self.root})
 
 def contructMapTrie(planetTuples):
     orbitMap = OrbitMap()
     for planetTuple in planetTuples:
         parentName = planetTuple[0]
         childName = planetTuple[1]
-        if parentName not in orbitMap.allPlanetNames and childName not in orbitMap.allPlanetNames:
+        planetNames = orbitMap.allPlanets.keys()
+        if parentName not in planetNames and childName not in planetNames:
             parentPlanet = Planet(parentName, None)
             childPlanet = Planet(childName, parentPlanet)
             parentPlanet.childPlanets.add(childPlanet)
+            childPlanet.parentPlanet = parentPlanet
 
-            orbitMap.allPlanetNames.add(parentName)
-            orbitMap.allPlanetNames.add((childName))
-            orbitMap.allTreeRoots.add(parentPlanet)
-        elif parentName in orbitMap.allPlanetNames and childName not in orbitMap.allPlanetNames:
+            orbitMap.allPlanets.update({parentName: parentPlanet})
+            orbitMap.allPlanets.update({childName: childPlanet})
+        elif parentName in planetNames and childName not in planetNames:
+            parentPlanet = orbitMap.allPlanets.get(parentName)
+            childPlanet = Planet(childName, parentPlanet)
+            parentPlanet.childPlanets.add(childPlanet)
+            childPlanet.parentPlanet = parentPlanet
 
-        elif parentName not in orbitMap.allPlanetNames and childName in orbitMap.allPlanetNames:
+            orbitMap.allPlanets.update({childName: childPlanet})
+        elif parentName not in planetNames and childName in planetNames:
+            parentPlanet = Planet(parentName, None)
+            childPlanet = orbitMap.allPlanets.get(childName)
+            parentPlanet.childPlanets.add(childPlanet)
+            childPlanet.parentPlanet = parentPlanet
 
+            orbitMap.allPlanets.update({parentName: parentPlanet})
         else: #both parent and child already on the map
-
+            parentPlanet = orbitMap.allPlanets.get(parentName)
+            childPlanet = orbitMap.allPlanets.get(childName)
+            parentPlanet.childPlanets.add(childPlanet)
+            childPlanet.parentPlanet = parentPlanet
     return orbitMap
 
 def getTotalPaths(orbitMap):
-    return 0
+    currentLevel = 0
+    levelPlanets = [orbitMap.root]
+    numOrbits = 0
+
+    while len(levelPlanets) != 0:
+        currentLevel += 1
+        lvlCopy = levelPlanets.copy()
+        for planet in lvlCopy:
+            levelPlanets.remove(planet)
+            for child in planet.childPlanets:
+                levelPlanets.append(child)
+        numOrbits += currentLevel * len(levelPlanets)
+    return numOrbits
 
 if __name__ == "__main__":
     with open("input.txt", 'r') as myfile:
