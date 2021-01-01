bc29d56cc4542a21916b3d2b1f0fc8fc74413db2
scarpenter7 <scarpenter7@outlook.com>
2020-12-31 17:49:50 -0800

    Day 7.2 Initial

--- 7.2/day7_2.py
+++ 7.2/day7_2.py
@@ -1,83 +1,78 @@
 import itertools
 
+#INFINITE LOOP BUG???
+
 class Machine(object):
     def __init__(self, codes, phaseSettings):
         codesCopy = codes.copy()
         self.amps = [Amp(codesCopy, phaseSettings[i], i) for i in range(5)]
-        self.currAmpIndex = 0
+        self.amps[0].input = 0
 
     def runMachine(self):
-        output = 0
-        box = OutputBox()
-
         while not self.amps[-1].halted:
-            for amp in self.amps:
-                amp.runAmp(output, box)
-            output = box.outputs[-1]
-        return output
+            for i, amp in enumerate(self.amps):
+                amp.runAmp()
+                if amp.output is not None:
+                    nextAmpIndex = (i + 1) % 5
+                    self.amps[nextAmpIndex].input = amp.output
+        return self.amps[-1].output
 
 class Amp(object):
     def __init__(self, codes, phaseSetting, ID):
         self.memory = codes
         self.phaseSetting = phaseSetting
+        self.phaseSettingUsed = False
         self.opCodeIndex = 0
+        self.input = None
         self.output = None
         self.ID = ID
         self.paused = True
         self.halted = False
 
-    def runAmp(self, input, box):
+    def runAmp(self):
         if self.halted:
             raise RuntimeError("This amplifier is halted!")
         self.paused = False
-        phaseSettingUsed = False
         while not self.halted and not self.paused:
             opCode = self.memory[self.opCodeIndex]
             modes = self.processOpCode(opCode)
             operation = modes[3:]
-            if not phaseSettingUsed:
-                useInput = self.phaseSetting
-                phaseSettingUsed = True
-            else:
-                useInput = input
-            self.opCodeIndex = self.executeOp(self.memory, useInput, modes, operation, self.opCodeIndex, box)
+            self.executeOp(operation, modes)
 
     def processOpCode(self, opCode):
         modes = [int(num) for num in str(opCode)]
-        while len(modes) < 5:
+        while len(modes) < 5: # Put 0's in front of the list until the list has size 5
             modes.insert(0, 0)
         return modes
 
-    def executeOp(self, memory, input, modes, operation, opCodeIndex, box):
-        # Returns new location of instruction ptr
-        param1 = memory[opCodeIndex + 1]
-        param2 = memory[opCodeIndex + 2]
-        param3 = memory[opCodeIndex + 3]
+    def executeOp(self, operation, modes):
+        param1 = self.memory[self.opCodeIndex + 1]
+        param2 = self.memory[self.opCodeIndex + 2]
+        param3 = self.memory[self.opCodeIndex + 3]
 
         if operation == [0, 1]:  # Add
-            return self.add(memory, param1, param2, param3, opCodeIndex, modes)
+            self.add(param1, param2, param3, modes)
         elif operation == [0, 2]:  # Multiply
-            return self.multiply(memory, param1, param2, param3, opCodeIndex, modes)
+            self.multiply(param1, param2, param3, modes)
         elif operation == [0, 3]:  # Store input at address
-            memory[param1] = input
-            return opCodeIndex + 2
-        elif operation == [0, 4]:  # Output element at address and store output in the output box
-            return self.storeOutput(memory, param1, box, opCodeIndex)
+            self.storeInput(param1)
+        elif operation == [0, 4]:  # Output element at address and store output
+            self.storeOutput(param1)
         elif operation == [0, 5]:  # Jump if true
-            return self.jumpIfTrue(memory, param1, param2, opCodeIndex, modes)
+            self.jumpIfTrue(param1, param2, modes)
         elif operation == [0, 6]:  # Jump if false
-            return self.jumpIfFalse(memory, param1, param2, opCodeIndex, modes)
+            self.jumpIfFalse(param1, param2, modes)
         elif operation == [0, 7]:  # Is less than
-            return self.isLessThan(memory, param1, param2, param3, opCodeIndex, modes)
+            self.isLessThan(param1, param2, param3, modes)
         elif operation == [0, 8]:  # equals
-            return self.equals(memory, param1, param2, param3, opCodeIndex, modes)
+            self.equals(param1, param2, param3, modes)
         elif operation == [0, 9]: # Halt
             self.halt()
-            return opCodeIndex
         else:
-            raise IndexError("Invalid OpCode: " + str(memory[opCodeIndex] + "\nOpCodeIndex: " + str(opCodeIndex)))
+            raise IndexError("Invalid OpCode: " + str(self.memory[self.opCodeIndex] +
+                                                      "\nOpCodeIndex: " + str(self.opCodeIndex)))
 
-    def getNumsFromModes(self, memory, params, modes):
+    def getNumsFromModes(self, params, modes):
         modesCopy = modes.copy()
         digitModes = modesCopy[:3]
         digitModes.reverse()
@@ -86,64 +81,78 @@ class Amp(object):
         for i, param in enumerate(params):
             num = None
             if digitModes[i] == 0:
-                num = memory[param]
+                num = self.memory[param]
             elif digitModes[i] == 1:
                 num = param
             nums.append(num)
 
         return nums
 
-    def add(self, memory, param1, param2, param3, opCodeIndex, modes):
-        nums = self.getNumsFromModes(memory, [param1, param2], modes)
-        memory[param3] = nums[0] + nums[1]
-        return opCodeIndex + 4
+    def add(self, param1, param2, param3, modes):
+        nums = self.getNumsFromModes([param1, param2], modes)
+        self.memory[param3] = nums[0] + nums[1]
+        self.opCodeIndex += 4
 
-    def multiply(self, memory, param1, param2, param3, opCodeIndex, modes):
-        nums = self.getNumsFromModes(memory, [param1, param2], modes)
-        memory[param3] = nums[0] * nums[1]
-        return opCodeIndex + 4
+    def multiply(self, param1, param2, param3, modes):
+        nums = self.getNumsFromModes([param1, param2], modes)
+        self.memory[param3] = nums[0] * nums[1]
+        self.opCodeIndex += 4
 
-    def storeOutput(self, memory, param1, box, opCodeIndex):
-        output = memory[param1]
+    def storeInput(self, param1):
+        if not self.phaseSettingUsed:
+            useInput = self.phaseSetting
+            self.phaseSettingUsed = True
+        else:
+            if self.input is None: #Need to pause this amp until we receive imput
+                self.pause()
+                return
+            else:
+                useInput = self.input
+        self.memory[param1] = useInput
+        self.opCodeIndex += 2
+
+    def storeOutput(self, param1):
+        output = self.memory[param1]
         # print(output)
-        box.outputs.append(output)
-        return opCodeIndex + 2
+        self.output = output
+        self.opCodeIndex += 2
 
-    def jumpIfTrue(self, codes, param1, param2, opCodeIndex, modes):
-        nums = self.getNumsFromModes(codes, [param1, param2], modes)
+    def jumpIfTrue(self, param1, param2, modes):
+        nums = self.getNumsFromModes([param1, param2], modes)
         if nums[0] != 0:
             return nums[1]
-        return opCodeIndex + 3
+        self.opCodeIndex += 3
 
-    def jumpIfFalse(self, codes, param1, param2, opCodeIndex, modes):
-        nums = self.getNumsFromModes(codes, [param1, param2], modes)
+    def jumpIfFalse(self, param1, param2, modes):
+        nums = self.getNumsFromModes([param1, param2], modes)
         if nums[0] == 0:
             return nums[1]
-        return opCodeIndex + 3
+        self.opCodeIndex += 3
 
-    def isLessThan(self, codes, param1, param2, param3, opCodeIndex, modes):
-        nums = self.getNumsFromModes(codes, [param1, param2], modes)
+    def isLessThan(self, param1, param2, param3, modes):
+        nums = self.getNumsFromModes([param1, param2], modes)
         if nums[0] < nums[1]:
-            codes[param3] = 1
+            self.memory[param3] = 1
         else:
-            codes[param3] = 0
-        return opCodeIndex + 4
+            self.memory[param3] = 0
+        self.opCodeIndex += 4
 
-    def equals(self, codes, param1, param2, param3, opCodeIndex, modes):
-        nums = self.getNumsFromModes(codes, [param1, param2], modes)
+    def equals(self, param1, param2, param3, modes):
+        nums = self.getNumsFromModes([param1, param2], modes)
         if nums[0] == nums[1]:
-            codes[param3] = 1
+            self.memory[param3] = 1
         else:
-            codes[param3] = 0
-        return opCodeIndex + 4
+            self.memory[param3] = 0
+        self.opCodeIndex += 4
+
+    def pause(self):
+        self.paused = True
+        print("Amp " + str(self.ID) + " paused.")
 
     def halt(self):
         self.halted = True
+        self.paused = True
         print("Amp " + str(self.ID) + " halted.")
-
-class OutputBox(object):
-    def __init__(self):
-        self.outputs = []
 
 
 if __name__ == "__main__":
@@ -155,7 +164,7 @@ if __name__ == "__main__":
     bestPhaseSettings = None
     for phaseSettings in phasePerms:
         machine = Machine(originalCodes, phaseSettings)
-        output = machine.runMachine(originalCodes, phaseSettings)
+        output = machine.runMachine()
         if output > maxOutput:
             maxOutput = output
             bestPhaseSettings = phaseSettings
